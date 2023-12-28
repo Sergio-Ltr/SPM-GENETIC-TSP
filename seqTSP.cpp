3d707cca4721c3927afe0ae42a205ab2fd5c5b3a
Sergio-WSL <sergio.ltr96@gmail.com>
2023-12-28 07:09:01 +0100

    Timer and Log Redirecting

--- seqTSP.cpp
+++ seqTSP.cpp
@@ -4,6 +4,10 @@
 #include <bits/stdc++.h>
 #include "utimer.hpp"
 
+// Global verobsity variables.
+bool print_logs = false;
+bool print_times = false;
+
 class City {     // The class
   public:       // Access specifier
     int id;    // Index in the .tsp file
@@ -17,6 +21,10 @@ class City {     // The class
     }
 
     float distance(City dest) { 
+        // if(print_times){
+        //      utimer single_euc_dist("Single Euclidean distance: ");
+        // }
+
         return pow(pow(x - dest.x, 2) + pow(y - dest.y, 2), 0.5);
     }
 };
@@ -47,22 +55,25 @@ class Route {
         ordering.assign(ordering_cities, ordering_cities + N);
     }
 
-    float totalDistance(){ //@TODO save values in an upper triangular (half of a symmetric) matrix??? 
+    float compute_total_distance(){ 
+        if(print_times){
+            utimer full_route_distance("Full route distance: ");
+        }
+
         distance = 0;
         for(int i=0; i < N; i++){
             float delta_d = cities->at(ordering[i]).distance(cities->at(ordering[(i+1)%N]));
             distance = distance + delta_d;
-
-            //std::cout << (cities->at(ordering[i])).id ;
-            //std::cout << " --> ";
-            //std::cout << (cities->at(ordering[(i+1)%N])).id ;
-            //std::cout << " == " <<  delta_d << '\n';
         }
 
         return distance;
+        //@TODO save values in an upper triangular (half of a symmetric) matrix??? 
     }
 
     Route PMX(Route mating_route, int crossing_point){
+        if(print_times){
+            utimer PMX("Breeding (PMX): ");
+        }
         if (crossing_point == -1){
             crossing_point = rand()%N;
         }
@@ -95,7 +106,10 @@ class Route {
     }      
 
     void mutate(){
-        // pick a random element
+        if(print_times){
+            utimer mutationTime("Mutation time:");
+        }
+
         int first = rand () % N;
         int second = rand () % (N-1);
 
@@ -109,7 +123,7 @@ class Route {
         ordering[second] = copy;
     } 
 
-    void printCitiesId () { 
+    void print_cities_ids () { 
         for(unsigned int i = 0; i < N; i++){
             std::cout << ordering[i] << '\n';
         }
@@ -165,17 +179,21 @@ class Nation {       // The class
         cities.push_back(city);
     }
 
-    City getCity(int id){ 
+    City get_city(int id){ 
         return cities[id];
     }
 
-    void printCitiesId () { 
+    void print_cities_ids () { 
         for(unsigned int i = 0; i < N; i++){
             std::cout << cities[i].id << '\n';
         }
     }
 
-    Route randomRoute() { 
+    Route random_route() { 
+        if(print_times){
+            utimer random_route_generation("Random route generation: ");
+        }
+
         return Route(N, &cities);
     }
 };
@@ -191,7 +209,7 @@ class Population {
 
     public: 
         int K; // Number of genes, a.k.a. routes 
-        Route bestRoute;
+        Route best_route;
         std::vector<Route> routes;
     
         Population(int k, Nation *nation){ 
@@ -199,67 +217,71 @@ class Population {
             N = nation->N;
 
             for (int i = 0; i< K; i++){
-                Route random_route = (*nation).randomRoute();
+                Route random_route = (*nation).random_route();
                 // ensure the distance is computed. 
                 routes.push_back(random_route);
                 probabilities.push_back(1/K);
             } 
 
             rank_all();
-            bestRoute = routes[0];
+            best_route = routes[0];
 
         }
 
         void rank_all() { 
-            float totalInverseDistance = 0;
+            if (print_times){
+                utimer ranking("Ranking (Sort + Normalize): ");
+            }
+            float total_inverse_distance = 0;
             for (int i = 0; i< K; i++){
-                totalInverseDistance += 1/routes[i].totalDistance(); // This also updates the distance value for the route object. 
+                total_inverse_distance += 1/routes[i].compute_total_distance(); // This also updates the distance value for the route object. 
             }
             
             std::sort(routes.begin(), routes.begin() + K, [](Route a, Route b){ return a.distance > b.distance; });  //bizarre cpp lambda function syntax
 
             for (int i = 0; i< K; i++){
-                probabilities[i] = (1/routes[i].distance)/totalInverseDistance;
+                probabilities[i] = (1/routes[i].distance)/total_inverse_distance;
             }
 
             picker = std::discrete_distribution<>(probabilities.begin(), probabilities.end());
+            return;
         }
 
-        Route pickRoute(){ 
+        Route pick_route(){ 
+            if (print_times){ 
+                utimer pick_route( "Partner pickup: ");
+            }
             return routes[picker(gen)]; 
         }
 
-        void evolve(float epsilon_mutation = 0){ 
+        void evolve(float mutation_rate = 0){ 
+            if (print_times){ 
+                utimer evolution("Single evolution loop (including ranking):");
+            }
+
             for (int i = 0; i < K; i++) { 
                 int crossing_point = rand()% N;//rand()%2 ==1 ? rand()% N : -1;
                 // Randomly choose which PMX ordering to choose
                 if(rand() % 2 == 1){
-                    routes[i] = routes[i].PMX(pickRoute(), crossing_point);
+                    routes[i] = routes[i].PMX(pick_route(), crossing_point);
                 } else { 
-                    routes[i] = pickRoute().PMX(routes[i], crossing_point);
+                    routes[i] = pick_route().PMX(routes[i], crossing_point);
                 }
 
                 // Randomly check if the mutation should happen or not. 
-                if ( epsilon_mutation != 0 && (rand() % int(1/epsilon_mutation)) == 1){ 
+                if ( mutation_rate != 0 && (rand() % int(1/mutation_rate)) == 1){ 
                     routes[i].mutate();
                 }
             }
 
             rank_all();
 
-            if (routes[0].totalDistance() < bestRoute.totalDistance()){ 
-                bestRoute.distance = routes[0].distance;
-                bestRoute.ordering = routes[0].ordering;
+            // Hold a copy of the best route ever found, in case evoltuion cause a distance increasement.
+            if (routes[0].compute_total_distance() < best_route.compute_total_distance()){ 
+                best_route.distance = routes[0].distance;
+                best_route.ordering = routes[0].ordering;
             } 
-            // else { 
-            //    routes[0].distance = bestRoute.distance;
-            //    routes[0].ordering = bestRoute.ordering;
-            // }
-
-            std::cout <<"Evolved" <<'\n';
         }
-
-    
 };
 
 
@@ -268,42 +290,78 @@ int main (int argc, char* argv[]){
     // The remaining lines just corrresponds to the coordinates of the cities in the nation.
     std::string filename = argc > 1 ? argv[1] : "wi29.tsp";
     std::cout << filename << '\n';
-    
-    int epochs = 500;
 
-    Nation nation = Nation(filename);    
-    Route best_route = nation.randomRoute();
+    // User can pass all the algorithm parameters. 
+    int epochs =  argc > 2 ? atoi(argv[2]) : 500;
+    std::cout << "Iterations: " << epochs << "\n";
+    int genes_num = argc > 3 ? atoi(argv[3]) : 1000;
+    std::cout << "Genes number: " << genes_num << "\n";
+    float mutation_rate = argc > 4 ? atof(argv[4]) : 0.0001;
+    std::cout << "Mutaton Rate: " << mutation_rate <<"\n";
 
-    Population genome(2500, &nation);
+    // Costumizable verbosity: 
+    // 2 ( > 1) for logs only
+    // 1 For logs and timers
+    // 0 for no prints at all
+    // -1 for timers only
+    int verbosity = argc > 5 ? atoi(argv[5]) : 0;
+    print_logs = verbosity > 0; 
+    print_times = verbosity == -1 || verbosity == 1;
 
-    utimer all("ALL: ");
-    for(int i = 0; i < epochs; i++ ){ 
-        genome.evolve(0.0001);
-        best_route = genome.bestRoute;
-        std::cout << "Epoch." << i << ")  Best Distance:";
-        std::cout << best_route.totalDistance() << '\n';
+    // Last param to decide wether to save logs on a file or just use the terminal. 
+    bool redirect_logs = argc > 5; 
+
+    if (redirect_logs){
+
+        std::time_t ct = std::time(0);
+        //TODO include a log folder and a t= 
+        std::stringstream sstm;
+        sstm << "LOGS/SEQ/E"<< epochs << "G"<< genes_num << "_" << ct << ".txt";
+        std::string logfile_path = sstm.str(); 
+        std::cout << "Trying redirecting the logs to :" << logfile_path << "\n";
+        std::freopen(logfile_path.c_str(),"w",stdout);
     }
     
-    best_route.printCitiesId();
+    Nation nation = Nation(filename);    
+    Route best_route = nation.random_route();
+
+    Population genome(genes_num, &nation);
+
+    if (print_times){
+        utimer overall("Overall execution time: ");
+    }
+    
+    for(int i = 0; i < epochs; i++ ){ 
+        genome.evolve(mutation_rate);
+        best_route = genome.best_route;
+
+        if(print_logs){ 
+            std::cout << "Epoch." << i << ")  Best Distance:";
+            std::cout << best_route.compute_total_distance() << '\n';
+        }
+    }
+    
+    best_route.print_cities_ids();
+    
     return 0;      
 }
 
 void lazyRouteTest(Nation nation){ 
-    nation.printCitiesId();
+    nation.print_cities_ids();
 
     std::cout << nation.N << '\n';
-    std::cout << nation.getCity(1).distance(nation.getCity(9))  << '\n';
-    std::cout << nation.randomRoute().totalDistance() << '\n';
+    std::cout << nation.get_city(1).distance(nation.get_city(9))  << '\n';
+    std::cout << nation.random_route().compute_total_distance() << '\n';
 
-    Route route1 =  nation.randomRoute();
-    Route route2 =  nation.randomRoute();
+    Route route1 =  nation.random_route();
+    Route route2 =  nation.random_route();
     
-    route1.printCitiesId();
+    route1.print_cities_ids();
     std::cout << "++++++++++++++++++++++++++++++++++++++++" << '\n';
-    route2.printCitiesId();
+    route2.print_cities_ids();
     std::cout << "========================================" << '\n';
 
     Route child = (route2.PMX(route1, 8));
-    child.printCitiesId();
+    child.print_cities_ids();
     std::cout << child.ordering[0] <<'\n';
 }
